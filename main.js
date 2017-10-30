edc1894dcec7ba8e78ff3e53d1471dc2fa8142d2
Basem <basem.morkos@hotmail.ca>
2017-10-30 10:25:09 -0400

    add MainNavigator - TabNavigator instance

--- main.js
+++ main.js
@@ -2,12 +2,31 @@ import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { TabNavigator } from 'react-navigation';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+
 class App extends React.Component {
+
   render() {
+    // TabNavigator(RouteConfigs, TabNavigatorConfig)
+    // RouteConfigs
+    const MainNavigator = TabNavigator(
+      {
+        welcome: { screen: WelcomeScreen },
+        auth: { screen: AuthScreen }
+      },
+      // TabNavigatorConfig
+      {
+        tabBarPosition: 'bottom',
+        animationEnabled: true,
+        tabBarOptions: { activeTintColor: '#e91e63' }      
+      }
+    );
+
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-        <Text>App is running!!!</Text>
+        <MainNavigator />
       </View>
     );
   }
