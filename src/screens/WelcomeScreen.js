f2a0b109f980e346dcdcfdbf6d5c50f2ed9b61d3
Basem <basem.morkos@hotmail.ca>
2018-01-04 13:48:49 -0500

    fixing lint errors in welcome screen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -6,46 +6,42 @@ import Slides from '../components/slides';
 import * as actions from '../actions/auth_actions';
 
 const SLIDE_DATA = [
-  { text: 'Welcome to JobApp', color: '#03A9F4' },
-  { text: 'Use this to get a job', color: '#009688' },
-  { text: 'Set your location, then swipe away', color: '#03A9F4' },
+	{ text: 'Welcome to JobApp', color: '#03A9F4' },
+	{ text: 'Use this to get a job', color: '#009688' },
+	{ text: 'Set your location, then swipe away', color: '#03A9F4' },
 ];
 
 class WelcomeScreen extends Component {
 	state = {
-	  token: null,
+		token: null,
 	}
 
 	async componentWillMount() {
-	  const token = await AsyncStorage.getItem('fb_token');
-	  console.log('token? ', token);
-	  if (token) {
-	    this.props.navigation.navigate('map');
-	    this.setState({ token });
-	  } else {
-	    this.setState({
-	      token: false,
-	    });
-	  }
+		const token = await AsyncStorage.getItem('fb_token');
+		if (token) {
+			this.props.navigation.navigate('map');
+			this.setState({ token });
+		} else {
+			this.setState({
+				token: false,
+			});
+		}
 	}
 
 	onSlidesComplete = () => {
-	  this.props.navigation.navigate('auth');
+		this.props.navigation.navigate('auth');
 	}
 
 	render() {
-	  if (this.state.token === null) {
-	    console.log('this.state.token is null');
-	    return <AppLoading />;
-	  }
-
-	  return (
-  <Slides
-    data={SLIDE_DATA}
-    onComplete={this.onSlidesComplete}
-  />
-	  );
-	  // return <AppLoading />
+		if (this.state.token === null) {
+			return <AppLoading />;
+		}
+		return (
+			<Slides
+				data={SLIDE_DATA}
+				onComplete={this.onSlidesComplete}
+			/>
+		);
 	}
 }
 
